451cb98cda8daa3ef3fa6ff76511721cd429b210
Nicole Liu <nicoleliu_@outlook.com>
2020-02-28 15:31:47 -0500

    Finished guard statements for my shape functions

--- AbstractionAndFunctions.playground/Pages/5 - Area Example - Rectangle.xcplaygroundpage/Contents.swift
+++ AbstractionAndFunctions.playground/Pages/5 - Area Example - Rectangle.xcplaygroundpage/Contents.swift
@@ -37,7 +37,10 @@ func areaOfRectangle(length: Double, width: Double) -> Double? {
 
 // Invoke the function... to find the area of a rectangle that is 10 units long and 5 units wide
 // Test case #1: length: 10, width: 5, result: 50
-let result = areaOfRectangle(length: 10, width: 5)
+if let result = areaOfRectangle(length: 10, width: 9) {
+    // Report the results
+    print("The area of that rectangle was \(result) square units.")
+}
 
 // Test case #2: length: 10.5, width: 10, result: 105
 areaOfRectangle(length: 10.5, width: 10)
@@ -48,8 +51,6 @@ areaOfRectangle(length: -5, width: 25)
 // Test case #4: length: 5, width: -25, result: nil
 areaOfRectangle(length: 5, width: -25)
 
-// Report the results
-print("The area of that rectangle was \(result) square units.")
 
 
 /*:
--- AbstractionAndFunctions.playground/Pages/7 - Authoring Functions.xcplaygroundpage/Contents.swift
+++ AbstractionAndFunctions.playground/Pages/7 - Authoring Functions.xcplaygroundpage/Contents.swift
@@ -33,7 +33,14 @@ import Foundation
 ///   - radius: radius of the cylinder
 ///   - height: height of the cylinder
 /// - Returns: the volume of the cyclinder
-func volumeCylinder(radius: Double, height: Double) -> Double {
+
+func volumeCylinder(radius: Double, height: Double) -> Double? {
+    // Ensure that we have reasonable values
+       guard radius > 0, height > 0 else {
+           // we have one or more bad inputs, so return a nill value
+           return nil
+       }
+    // we have reasonable values so return the area
     return Double.pi * height * pow(radius, 2.0)
 }
 
@@ -43,7 +50,13 @@ volumeCylinder(radius: 5.0, height: 6.0)
 /// Find the surface area of a sphere
 /// - Parameter radius: radius of the sphere
 /// - Returns: the surface area of the sphere
-func surfaceAreaSphere(radius: Double) -> Double {
+func surfaceAreaSphere(radius: Double) -> Double? {
+    // Ensure that we have reasonable values
+       guard radius > 0 else {
+           // we have one or more bad inputs, so return a nill value
+           return nil
+       }
+    // we have reasonable values so return the area
     return 4 * Double.pi * pow(radius, 2.0)
 }
 
@@ -52,7 +65,13 @@ surfaceAreaSphere(radius:5.0)
 /// Find the volume of a sphere
 /// - Parameter radius: the radius of the sphere
 /// - Returns: the volume of the sphere
-func volumeSphere(radius: Double) -> Double {
+func volumeSphere(radius: Double) -> Double? {
+    // Ensure that we have reasonable values
+       guard radius > 0 else {
+           // we have one or more bad inputs, so return a nill value
+           return nil
+       }
+    // we have reasonable values so return the area
     return 4/3 * Double.pi * pow(radius, 3.0)
 }
 
@@ -66,11 +85,17 @@ volumeSphere(radius:5.0)
 ///   - height: the height of the trapezoid
 /// - Returns: the area of the trapezoid
 
-func areaTrapezoid(a: Double, b: Double, height: Double) -> Double {
+func areaTrapezoid(a: Double, b: Double, height: Double) -> Double? {
+    // Ensure that we have reasonable values
+       guard a > 0, b > 0, height > 0 else {
+           // we have one or more bad inputs, so return a nill value
+           return nil
+       }
+    // we have reasonable values so return the area
     return 1/2 * (a+b) * height
 }
 
-areaTrapezoid(a: 5.0, b: 10.0, height:8.0)
+areaTrapezoid(a: 5.0, b: 10.0, height: 8.0)
 
 
 /// Find the perimeter of a triangle
@@ -79,7 +104,12 @@ areaTrapezoid(a: 5.0, b: 10.0, height:8.0)
 ///   - b: second side length
 ///   - c: third side length
 /// - Returns: the perimeter of a triangle
-func perimeterTriangle(a: Double, b:Double, c: Double) -> Double {
+func perimeterTriangle(a: Double, b:Double, c: Double) -> Double? { // Ensure that we have reasonable values
+       guard a > 0, b > 0, c > 0 else {
+           // we have one or more bad inputs, so return a nill value
+           return nil
+       }
+    // we have reasonable values so return the area
     return a+b+c
 }
 
@@ -91,7 +121,13 @@ perimeterTriangle(a: 3, b:4, c: 5)
 ///   - length: the length of the rectangle
 ///   - width: the width of the rectangle
 /// - Returns: the perimeter of the rectangle
-func perimeterRectangle(length l: Double, width w: Double) -> Double {
+func perimeterRectangle(length l: Double, width w: Double) -> Double? {
+    // Ensure that we have reasonable values
+       guard l > 0, w > 0 else {
+           // we have one or more bad inputs, so return a nill value
+           return nil
+       }
+    // we have reasonable values so return the area
     return 2*(l+w)
 }
 
@@ -101,16 +137,30 @@ func perimeterRectangle(length l: Double, width w: Double) -> Double {
 ///   - b: base length of the pyramid
 ///   - s: side length of the pyramid
 /// - Returns: the surface area of the pyramid
-func surfaceAreaPyramid(base b: Double, sideHeight s: Double) -> Double {
-    return pow(b, 2.0) + (2(b*s))
+func surfaceAreaPyramid(base b: Double, slantHeight s: Double) -> Double? {
+    // Ensure that we have reasonable values
+       guard b > 0, s > 0 else {
+           // we have one or more bad inputs, so return a nill value
+           return nil
+       }
+    // we have reasonable values so return the area
+return pow (b,2) + 2*b*s
 }
+
+surfaceAreaPyramid (base: 5, slantHeight: 10)
 
 
 /// Find the area of parallelogram
 /// - Parameters:
 ///   - b: base length of the parallelogram
 ///   - h: height of the parallelogram
-func areaParallelogram(baseLength b: Double, height h: Double) -> Double {
+func areaParallelogram(baseLength b: Double, height h: Double) -> Double? {
+    // Ensure that we have reasonable values
+       guard b > 0, h > 0 else {
+           // we have one or more bad inputs, so return a nill value
+           return nil
+       }
+    // we have reasonable values so return the area
     return b*h
 }
 
